ce592a1cf9eb41818a9d2979d21362f38d23cead
chrisbmineiro <chrisbmineiro@gmail.com>
2025-01-13 20:31:24 -0300

    feat: implementando integração com GPT da OpenAI utilizando Spring AI

--- src/main/java/com/cbm/ecomart/controller/GeradorController.java
+++ src/main/java/com/cbm/ecomart/controller/GeradorController.java
@@ -1,5 +1,6 @@
 package com.cbm.ecomart.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/gerador")
 public class GeradorController {
 
+    private final ChatClient chatClient;
+
+    public GeradorController (ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
     @GetMapping
     public String gerarProdutos() {
-        return "Gerando...";
+        var userInput = "Gerar 5 produtos ecologicos";
+
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
     }
 }
