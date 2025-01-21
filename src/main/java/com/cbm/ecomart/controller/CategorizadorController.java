17d2d8b0b5e41cc2e78d34bbce5d58d4d3f778ca
chrisbmineiro <chrisbmineiro@gmail.com>
2025-01-20 21:23:30 -0300

    feat: implementando CategorizadorController para categorizar produtos

---
+++ src/main/java/com/cbm/ecomart/controller/CategorizadorController.java
@@ -0,0 +1,45 @@
+package com.cbm.ecomart.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptionsBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorizador")
+public class CategorizadorController {
+
+    private final ChatClient chatClient;
+
+    public CategorizadorController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping
+    public String categorizarProdutos(String produto) {
+        var system = """
+                Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
+                
+                Escolha uma categoria dentro da lista abaixo:
+                1. Higiene pessoal
+                2. Eletrônicos
+                3. Esportes
+                4. Outros
+                
+                ###### exemplo de uso:
+                
+                Pergunta: Bola de futebol
+                Resposta: Esportes
+                """;
+
+        return this.chatClient.prompt()
+                .system(system)
+                .user(produto)
+                .options(ChatOptionsBuilder.builder()
+                        .withTemperature(0.85)
+                        .build())
+                .call()
+                .content();
+    }
+}
